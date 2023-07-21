136b31c97890b143f1efc41e56119965ce6531e8
TerryW624 <terrywade634@gmail.com>
2023-07-21 12:01:15 -0700

    feat: functions are set up

---
+++ src/Components/mathFunctions.js
@@ -0,0 +1,17 @@
+function addTwoNumbers(num1, num2) {
+    // This will add num1 and num2 and return the value
+}
+
+function divTwoNumbers(num1, num2) {
+    // This will decide the divisor and the dividend based off of a comparison operator
+    // and then divide
+}
+
+function multTwoNumbers(num1, num2) {
+    //This will multiply two numbers
+}
+
+function diffTwoNumbers(num1, num2) {
+    // This will decide the minuend and the subtrahend and based off of a comparison
+    // operator and then subtract.
+}
---
+++ src/Components/stringFunctions.js
@@ -0,0 +1,11 @@
+function capFirstLetter(string) {
+    // This will capitalize the fLetter of "string"
+}
+
+function catTwoStrings(string1, string2) {
+    // This will concatenate two strings
+}
+
+function secWord(sentenceString) {
+    //This will return the second word in a sentence string
+}
