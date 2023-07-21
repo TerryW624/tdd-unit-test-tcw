4cf2f12aa8c7ff6465d168ca9e4cab6d63c909d7
TerryW624 <terrywade634@gmail.com>
2023-07-21 13:50:21 -0700

    MVP achieved

--- src/Components/stringFunctions.js
+++ src/Components/stringFunctions.js
@@ -1,13 +1,18 @@
 function capFirstLetter(string) {
     // This will capitalize the fLetter of "string"
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function catTwoStrings(string1, string2) {
     // This will concatenate two strings
+    return string1 + " " + string2;
 }
 
 function secWord(sentenceString) {
     //This will return the second word in a sentence string
+    let secWordBeginning = sentenceString.indexOf(" ");
+    let secWordEnding = sentenceString.indexOf(" ", secWordBeginning + 1);
+    return sentenceString.substring(secWordBeginning + 1, secWordEnding);
 }
 
-export {capFirstLetter, catTwoStrings, secWord}
+export {capFirstLetter, catTwoStrings, secWord};
