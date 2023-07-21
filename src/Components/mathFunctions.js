f7d9504b0edb43a591906efd25d383615996fba2
TerryW624 <terrywade634@gmail.com>
2023-07-21 13:12:40 -0700

    feat: passed all math function tests

--- src/Components/mathFunctions.js
+++ src/Components/mathFunctions.js
@@ -1,19 +1,31 @@
 function addTwoNumbers(num1, num2) {
     // This will add num1 and num2 and return the value
+    return num1 + num2
 }
 
 function divTwoNumbers(num1, num2) {
     // This will decide the divisor and the dividend based off of a comparison operator
     // and then divide
+    if (num1 > num2) {
+        return num1/num2
+    } else {
+        return num2/num1
+    }
 }
 
 function multTwoNumbers(num1, num2) {
     //This will multiply two numbers
+    return num1 + num2
 }
 
 function diffTwoNumbers(num1, num2) {
     // This will decide the minuend and the subtrahend and based off of a comparison
     // operator and then subtract.
+    if (num1 > num2) {
+        return num1 - num2
+    } else {
+        return num2 - num1
+    }
 }
 
 export {addTwoNumbers, divTwoNumbers, multTwoNumbers, diffTwoNumbers}
--- src/Components/mathFunctions.test.js
+++ src/Components/mathFunctions.test.js
@@ -1,10 +1,10 @@
 import { addTwoNumbers, divTwoNumbers, multTwoNumbers, diffTwoNumbers } from "./mathFunctions";
 
 describe("two plus two is four", () =>{
-    test("two plus two is four", () => {
+    test("2 + 2 = 4", () => {
         expect(addTwoNumbers(2, 2)).toBe(4);
     })
-    test("two plus two is not three", () => {
+    test("2 + 2 != 3", () => {
         expect(addTwoNumbers(2, 2)).not.toBe(3);
     })
 })
@@ -13,13 +13,13 @@ describe("four divided by two is two", () =>{
     test("4/2 = 2", () => {
         expect(divTwoNumbers(4, 2)).toBe(2);
     })
-    test("4/2 = 2", () => {
+    test("for num1 === 2; 4/2 = 2", () => {
         expect(divTwoNumbers(2, 4)).toBe(2);
     })
-    test("4/2 = 2", () => {
+    test("4/2 != 3", () => {
         expect(divTwoNumbers(4, 2)).not.toBe(3);
     })
-    test("4/2 = 2", () => {
+    test("for num1 === 2; 4/2 != 2/4", () => {
         let notTrue = 2/4;
         expect(divTwoNumbers(2, 4)).not.toBe(notTrue); 
     })
@@ -30,7 +30,7 @@ describe("two multiplied by two is four", () =>{
     test("2 * 2 = 4", () => {
         expect(multTwoNumbers(2, 2)).toBe(4);
     })
-    test("2 * 2 = 3", () => {
+    test("2 * 2 != 3", () => {
         expect(multTwoNumbers(2, 2)).not.toBe(3)
     })
 })
@@ -39,13 +39,13 @@ describe("four minus two is two", () =>{
     test("4 - 2 = 2", () => {
         expect(divTwoNumbers(4, 2)).toBe(2);
     })
-    test("4 - 2 = 2", () => {
+    test("for num1 === 2; 4 - 2 = 2", () => {
         expect(divTwoNumbers(2, 4)).toBe(2);
     })
     test("4 - 2 = 2", () => {
         expect(divTwoNumbers(4, 2)).not.toBe(3);
     })
-    test("4 - 2 = 2", () => {
+    test("for num1 === 2; 4 - 2 = 2", () => {
         let notTrue = 2 - 4;
         expect(divTwoNumbers(2, 4)).not.toBe(notTrue);
     })
